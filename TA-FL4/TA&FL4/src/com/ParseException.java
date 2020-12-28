06b80d1b5ea094bc5b718b7c78a591bba5ede8fd
IgorPigeon <50440388+IgorPigeon@users.noreply.github.com>
2020-12-28 08:32:46 +0300

    Add files via upload

---
+++ TA-FL4/TA&FL4/src/com/ParseException.java
@@ -0,0 +1,9 @@
+package com;
+
+public class ParseException extends Exception {
+    public final int index;
+    public ParseException(String message, int index) {
+        super(message);
+        this.index = index;
+    }
+}
